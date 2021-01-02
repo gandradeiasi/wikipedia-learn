68978a758cf06c771d5f99288e61eaef934e4fea
Gabriel Iasi <31139106+gandradeiasi@users.noreply.github.com>
2021-01-02 19:32:47 -0300

    Comportamento de botões e cursor

--- src/js/eventos-keydown.js
+++ src/js/eventos-keydown.js
@@ -1,17 +1,19 @@
 document.addEventListener('keydown', e => {
-    if (e.key == "-") {
-        botoesRemover[0].click();
-    }
-    else if (e.key == "+") {
-        botoesAprovar[0].click();
-    }
-    else if (e.key == "*") {
-        botoesComentar[0].click();
-    }
-    else if (e.key == "Escape") {
-        cancelarComentario.click();
-    }
-    else if (e.key == "=") {
-        document.querySelector('a').click();
+    if (!modalComentario.classList.contains('active')) {
+        if (e.key == "-") {
+            botoesRemover[0].click();
+        }
+        else if (e.key == "+") {
+            botoesAprovar[0].click();
+        }
+        else if (e.key == "*") {
+            botoesComentar[0].click();
+        }
+        else if (e.key == "Escape") {
+            cancelarComentario.click();
+        }
+        else if (e.key == "=") {
+            document.querySelector('a').click();
+        }
     }
 })
